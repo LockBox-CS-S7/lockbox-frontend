150e2572848cccac3ea7129e92185b91f0534ff6
rikdgd <74911706+rikdgd@users.noreply.github.com>
2024-11-18 11:51:40 +0100

    file upload now uses the userId based on the logged in user.

--- src/pages/files/upload-file.tsx
+++ src/pages/files/upload-file.tsx
@@ -1,57 +1,64 @@
 import { FormEvent } from 'react';
+import { useAuth0 } from "@auth0/auth0-react";
 import axios from 'axios';
-// import './file-upload.css';
+import { PageLayout } from "../../components/page-layout";
+import './upload-file.css';
 
 
 function UploadFile() {
+    const { user, isAuthenticated, isLoading } = useAuth0();
+    
+    if (isLoading) return <div>Loading...</div>
+    if (!isAuthenticated) return <div>You need to be authenticated for this page.</div>
+    
+    
+    async function handleFormSubmit(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        
+        const formData = new FormData(event.currentTarget);
+        console.log(formData);
+        const userId = user?.sub;
+        const file = formData.get('file') as File | null;
+        
+        
+        if (!userId) {
+            console.error('no user id found');
+            return;
+        }
+        if (!file) {
+            console.error('no file selected');
+            return;
+        }
+        
+        const fileContents = getFileContents(file);
+        //ToDo: encrypt fileContents before sending
+        
+        const postForm = new FormData();
+        postForm.append('user_id', userId);
+        postForm.append('file', file);
+        
+        try {
+            await axios.postForm('http://localhost:8080/api/', postForm);
+        } catch {
+            console.error('Failed to send file to backend.');
+        }
+    }
+    
+    
     return (
-        <div className="file-upload-container">
-            <h2>Upload a file</h2>
-            
-            <form className="upload-form" onSubmit={handleFormSubmit}>
-                <label>user id</label>
-                <input id="user_id" name="user_id" type="text"/>
+        <PageLayout>
+            <div className="file-upload-container">
+                <h2 className='title'>Upload a file</h2>
                 
-                <label>select file</label>
-                <input id="file" name="file" type="file"/>
-                
-                <input type="submit" value="submit" className='submit-btn'/>
-            </form>
-        </div>
+                <form className="upload-form" onSubmit={handleFormSubmit}>
+                    <label>select file</label>
+                    <input id="file" name="file" type="file"/>
+                    
+                    <input type="submit" value="upload" className='submit-btn'/>
+                </form>
+            </div>
+        </PageLayout>
     );
-}
-
-
-async function handleFormSubmit(event: FormEvent<HTMLFormElement>) {
-    event.preventDefault();
-    
-    const formData = new FormData(event.currentTarget);
-    console.log(formData);
-    const userId = formData.get('user_id') as string | null;
-    const file = formData.get('file') as File | null;
-    
-    
-    if (!userId) {
-        console.error('no user id entered');
-        return;
-    }
-    if (!file) {
-        console.error('no file selected');
-        return;
-    }
-    
-    const fileContents = getFileContents(file);
-    //ToDo: encrypt fileContents before sending
-    
-    const postForm = new FormData();
-    postForm.append('user_id', userId);
-    postForm.append('file', file);
-    
-    try {
-        await axios.postForm('http://localhost:8080/api/', postForm);
-    } catch {
-        console.error('Failed to send file to backend.');
-    }
 }
 
 
